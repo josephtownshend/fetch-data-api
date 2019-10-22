4ee055b37589adc9f62a5f534944a9cca7acf103
joseph townshend <joetownshend@mac.com>
2019-10-22 16:41:03 +0100

    chores: commented code

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 export default class FetchRandomUser extends React.Component {
 
+// state 'loading' is true by default.
+// state 'person' is null by default as we havent requested one on page load.
   state = {
     loading: true,
     person: null
   };
 
+// this is fetching data.
+// we are making this function asynchronous as we have used 'fetch'.
   async componentDidMount() {
+    // storing our URL in a const.
     const url = "https://api.randomuser.me/";
+    // this is 'fetching' our data and storing it in 'response'.
+    // 'fetch' is an asynchronous function so we need to use 'await'.
+    // it will wait for the 'async' call to finish then store in 'response'.
     const response = await fetch(url);
+    // we are then getting the 'json' data from the 'response'.
+    // as 'fetch' is async we use 'await'.
     const data = await response.json();
+    // setting state of 'person' here and changing loading to 'false'
     this.setState({ person: data.results[0], loading: false })
   }
 
