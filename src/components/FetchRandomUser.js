a886b1926206891a627ff89870f077ba54aeb18a
joseph townshend <joetownshend@mac.com>
2019-10-22 13:08:20 +0100

    feat: componentDidMount setup returning loading...

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export default class FetchRandomUser extends React.Component {
+
+  state = {
+    loading: true
+  }
+
+
+  componentDidMount() {
+
+  }
+
   render() {
-    return <div>user...</div>
+    return <div>
+      {this.state.loading ? <div>loading...</div> : <div>person...</div>}
+    </div>;
   }
 }
