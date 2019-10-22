6ab976f1c1ebc3ce5810554adb78fd157cc3105a
joseph townshend <joetownshend@mac.com>
2019-10-22 13:18:27 +0100

    feat: returning first name

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -4,7 +4,7 @@ export default class FetchRandomUser extends React.Component {
 
   state = {
     loading: true,
-    person: null,
+    person: null
   };
 
 
@@ -12,8 +12,7 @@ export default class FetchRandomUser extends React.Component {
     const url = "https://api.randomuser.me/";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({person: data.results[0]})
-    console.log(data);
+    this.setState({ person: data.results[0], loading: false })
   }
 
 
@@ -21,7 +20,13 @@ export default class FetchRandomUser extends React.Component {
   render() {
     return (
       <div>
-        {this.state.loading ? <div>loading...</div> : <div>person...</div>}
+        {this.state.loading || !this.state.person ?  (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div>{this.state.person.name.first}</div>
+          </div>
+        )}
       </div>
     );
   }
