16c80117c699e9c29292c2f22a073a5028f85f6a
joseph townshend <joetownshend@mac.com>
2019-10-23 09:05:30 +0100

    refactor: change key to use index of map

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -37,8 +37,8 @@ export default class FetchRandomUser extends React.Component {
 
     return (
       <div>
-        {this.state.people.map(person => (
-          <div key={person.login.uunid}>
+        {this.state.people.map((person, i) => (
+          <div key={`some-person-#{i}`}>
             <div>{person.name.title}</div>
             <div>{person.name.first}</div>
             <div>{person.name.last}</div>
